709ea657ea8c48d2b2ba58e1724ed3afd7be979a
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-05-26 13:40:27 +0300

    Updates

---
+++ static/js/main.84f5312f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpskullcandy=this.webpackJsonpskullcandy||[]).push([[0],[function(s,n,c){"use strict";c.r(n);c(1)},function(s,n,c){}],[[0,1]]]);
+//# sourceMappingURL=main.84f5312f.chunk.js.map
