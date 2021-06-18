40ab95fa8e50d0a67f965fd63553b9cbca010b9d
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-06-18 14:47:31 +0300

    Change styles for insta and start

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Start.scss';
+
+import { Context } from '../../context';
 
 import Wrapper from '../Wrapper/Wrapper';
 
 import data from './data.js';
 
 function Stas({ number }) {
+
+    const { isMobile } = useContext(Context);
 
     const dataStart = data[number];
     const cls = ['skullcandy-start', 'skullcandy-' + dataStart.id];
@@ -21,22 +25,25 @@ function Stas({ number }) {
     return (
         <Wrapper>
             <div className={cls.join(' ')}>
-                <div className="skullcandy-start-insta">
-                    <div className="skullcandy-start-insta-img">
-                        <img src={dataStart.insta} alt={dataStart.nickname} />
+                {!isMobile && (
+                    <div className="skullcandy-start-insta">
+                        <div className="skullcandy-start-insta-img">
+                            <img src={dataStart.insta} alt={dataStart.nickname} />
+                        </div>
+                        <div className="skullcandy-start-insta-title">
+                            <h6>{dataStart.nickname}</h6>
+                            <a
+                                href={dataStart.instaLink}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="skullcandy-btn"
+                            >
+                                <span>Подписаться</span>
+                            </a>
+                        </div>
                     </div>
-                    <div className="skullcandy-start-insta-title">
-                        <h5>{dataStart.nickname}</h5>
-                        <a
-                            href={dataStart.instaLink}
-                            target="_blank"
-                            rel="noreferrer"
-                            className="skullcandy-btn"
-                        >
-                            <span>Подписаться</span>
-                        </a>
-                    </div>
-                </div>
+                )}
+
                 <div className="skullcandy-start-content skullcandy-left">
                     <h2>
                         <span>{dataStart.name}</span>
@@ -44,6 +51,24 @@ function Stas({ number }) {
                     <div className="skullcandy-start-content-subtitle">
                         <span dangerouslySetInnerHTML={{ __html: dataStart.subtitle }}></span>
                     </div>
+                    {isMobile && (
+                        <div className="skullcandy-start-insta">
+                            <div className="skullcandy-start-insta-img">
+                                <img src={dataStart.insta} alt={dataStart.nickname} />
+                            </div>
+                            <div className="skullcandy-start-insta-title">
+                                <h6>{dataStart.nickname}</h6>
+                                <a
+                                    href={dataStart.instaLink}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="skullcandy-btn"
+                                >
+                                    <span>Подписаться</span>
+                                </a>
+                            </div>
+                        </div>
+                    )}
                     <h5>
                         <span dangerouslySetInnerHTML={{ __html: dataStart.h5 }}></span>
                     </h5>
